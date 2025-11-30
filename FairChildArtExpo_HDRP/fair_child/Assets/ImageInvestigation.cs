2466dd7017aab4de22c9bc25189eb355ceadfadd
PBChandler <prestonbchandler@gmail.com>
2025-11-30 16:37:41 -0500

    got the repo working again, last couple weeks of work.

---
+++ FairChildArtExpo_HDRP/fair_child/Assets/ImageInvestigation.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ImageInvestigation : MonoBehaviour
+{
+    public GameObject bg, hiddenElement;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
